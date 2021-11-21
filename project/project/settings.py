f0efd514cc528ff47c45b6d9c6c8ef8e19f229f3
Mohamed Mohamed <studio@mohd.ca>
2021-11-20 19:57:10 -0500

    heroku setup

--- project/project/settings.py
+++ project/project/settings.py
@@ -173,7 +173,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'coreapp.social_auth_pipeline.create_user_by_type',  # <--- set the path to the function
+    'coreapp.social_auth_pipeline.create_user_by_type',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
